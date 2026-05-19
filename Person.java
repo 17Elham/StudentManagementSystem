e82d5105c4b1fe765d4e25445b66106b3008a21b
17Elham <159579586+17Elham@users.noreply.github.com>
2026-05-20 01:18:27 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,45 @@
+package studentmanagementsystem;
+
+/**
+ * Abstract class representing a general Person in the system.
+ * This class applies Abstract Classes and Encapsulation concepts.
+ */
+public abstract class Person {
+    // Attributes encapsulated using private access modifier
+    private int id;
+    private String name;
+    private String email;
+
+    // Constructor to initialize properties
+    public Person(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters for Encapsulation
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public abstract void displayDetails();
+}
