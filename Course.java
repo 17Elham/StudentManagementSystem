d60f91dffb94f9c392c9cb54e0f79a4b50739d51
17Elham <159579586+17Elham@users.noreply.github.com>
2026-05-20 19:36:29 +0200

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,46 @@
+package studentmanagementsystem;
+
+public class Course {
+    private String courseCode;
+    private String courseName;
+    private int creditHours;
+
+    // Constructor
+    public Course(String courseCode, String courseName, int creditHours) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.creditHours = creditHours;
+    }
+
+    // Getters and Setters
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    // Method to display course details
+    public void displayCourseInfo() {
+        System.out.println("Course Code: " + courseCode);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Credit Hours: " + creditHours);
+    }
+}
